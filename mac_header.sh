aee77804ae916391c08264a4306e45b49c20e336
Tatsuya Imai <34620490+101ta28@users.noreply.github.com>
2022-02-10 15:42:11 +0900

    Create mac_header.sh

---
+++ mac_header.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# ./mac_header.sh
+
+# Create Directory
+mkdir -p ~/.vim/plugin
+
+# Add stdheader to vim plugins
+curl curl https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim > ~/.vim/plugin/stdheader.vim
+
+read -p "Input your 42UserName> " username
+
+echo "let g:user42 = '$username'" >> ~/.vimrc
+echo "let g:mail42 = '$username@student.42tokyo.jp'" >> ~/.vimrc
